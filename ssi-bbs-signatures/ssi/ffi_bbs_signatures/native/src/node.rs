62de4930272a0fb5b7be07f925d9e5bed0266f71
Andrew Lord <andrew.lord@appliedblockchain.com>
2022-05-12 13:16:40 +0100

    :sparkles: (ffi_bbs) Add `bbs_sign` Neon binding

--- ssi-bbs-signatures/ssi/ffi_bbs_signatures/native/src/node.rs
+++ ssi-bbs-signatures/ssi/ffi_bbs_signatures/native/src/node.rs
@@ -142,6 +142,52 @@ fn node_bls_public_key_to_bbs_key(mut cx: FunctionContext) -> JsResult<JsArrayBu
   Ok(slice_to_js_array_buffer!(&pk_bytes, cx))
 }
 
+/// Generate a BBS+ signature
+/// The first argument is the domain separation label
+/// The second argument is the private key `x` created from bls_generate_key.
+/// The remaining values are the messages to be signed.
+/// If no messages are supplied, an error is thrown.
+///
+/// `signature_context`: `Object` the context for the signature creation
+/// The context object model is as follows:
+/// {
+///     "secretKey": ArrayBuffer                // The private key of signer
+///     "publicKey": ArrayBuffer                // The public key of signer
+///     "messages": [ArrayBuffer, ArrayBuffer], // The messages to be signed as ArrayBuffers. They will be hashed with Blake2b
+/// }
+///
+/// `return`: `ArrayBuffer` the signature
+fn node_bbs_sign(mut cx: FunctionContext) -> JsResult<JsArrayBuffer> {
+  let js_obj = cx.argument::<JsObject>(0)?;
+
+  let sk = SecretKey::from(obj_property_to_fixed_array!(
+      &mut cx,
+      js_obj,
+      "secretKey",
+      0,
+      FR_COMPRESSED_SIZE
+  ));
+
+  let pk_bytes = obj_property_to_slice!(&mut cx, js_obj, "publicKey");
+  let pk = PublicKey::from_bytes_compressed_form(pk_bytes.as_slice()).unwrap();
+
+  if pk.validate().is_err() {
+      panic!("Invalid key");
+  }
+
+  let message_bytes = obj_property_to_vec!(&mut cx, js_obj, "messages");
+  let mut messages = Vec::new();
+  for i in 0..message_bytes.len() {
+      let message = js_array_buffer_to_slice!(&mut cx, message_bytes[i]);
+      messages.push(SignatureMessage::hash(message));
+  }
+
+  let signature = handle_err!(Signature::new(messages.as_slice(), &sk, &pk));
+  let result = slice_to_js_array_buffer!(&signature.to_bytes_compressed_form()[..], cx);
+
+  Ok(result)
+}
+
 register_module!(mut cx, {
   cx.export_function("bls_generate_blinded_g1_key", node_bls_generate_blinded_g1_key)?;
   cx.export_function("bls_generate_blinded_g2_key", node_bls_generate_blinded_g2_key)?;
@@ -149,5 +195,6 @@ register_module!(mut cx, {
   cx.export_function("bls_generate_g2_key", node_bls_generate_g2_key)?;
   cx.export_function("bls_secret_key_to_bbs_key", node_bls_secret_key_to_bbs_key)?;
   cx.export_function("bls_public_key_to_bbs_key", node_bls_public_key_to_bbs_key)?;
+  cx.export_function("bbs_sign", node_bbs_sign)?;
   Ok(())
 });
