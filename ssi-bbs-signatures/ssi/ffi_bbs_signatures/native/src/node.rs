da06c0f2f4ae1e53eac62646174ac572706b0f79
Andrew Lord <andrew.lord@appliedblockchain.com>
2022-05-12 12:21:58 +0100

    :sparkles: (ffi_bbs) Add `bls_generate_g1_key` Neon binding

--- ssi-bbs-signatures/ssi/ffi_bbs_signatures/native/src/node.rs
+++ ssi-bbs-signatures/ssi/ffi_bbs_signatures/native/src/node.rs
@@ -43,8 +43,21 @@ fn node_bls_generate_blinded_g2_key(mut cx: FunctionContext) -> JsResult<JsObjec
 
   Ok(blinded_key_values_to_object!(cx, sk_bytes, pk_bytes, bf_bytes))
 }
+
+/// Generate a BLS key pair where secret key `x` in Fp
+/// and public key `w` = `g1` ^ `x`
+/// `seed`: `ArrayBuffer` [opt]
+/// `return`: Object { publicKey: `ArrayBuffer`, secretKey: `ArrayBuffer` }
+fn node_bls_generate_g1_key(mut cx: FunctionContext) -> JsResult<JsObject> {
+  let seed = arg_to_opt_slice!(cx, 0);
+
+  let (pk_bytes, sk_bytes) = bls_generate_g1_key(seed);
+
+  Ok(key_values_to_object!(cx, sk_bytes, pk_bytes))
+}
 register_module!(mut cx, {
   cx.export_function("bls_generate_blinded_g1_key", node_bls_generate_blinded_g1_key)?;
   cx.export_function("bls_generate_blinded_g2_key", node_bls_generate_blinded_g2_key)?;
+  cx.export_function("bls_generate_g1_key", node_bls_generate_g1_key)?;
   Ok(())
 });
