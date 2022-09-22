c4c30c38159b70f2db492803bdd4ea45848a5860
Andrew Lord <andrew.lord@appliedblockchain.com>
2022-09-22 15:00:28 +0100

    chore: update C header build settings to match existing package

--- native/build.rs
+++ native/build.rs
@@ -14,8 +14,11 @@ fn main() {
         // generate C header
         if let Ok(crate_dir) = env::var("CARGO_CFG_MANIFEST_DIR") {
           cbindgen::Builder::new()
-            .with_crate(crate_dir)
+            .with_crate_and_name(crate_dir, "ffi-bbs-signatures")
             .with_language(cbindgen::Language::C)
+            .with_include_guard("__bbs__plus__included__")
+            .with_parse_deps(true)
+            .with_parse_include(&["ffi-support"])
             .generate()
             .expect("Unable to generate bindings")
             .write_to_file("../wrappers/c/libbbs.h");
