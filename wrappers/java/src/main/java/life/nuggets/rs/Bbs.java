1529d24493ceff7c5345b6f044487b557cda6964
Andrew Lord <andrew.lord@appliedblockchain.com>
2022-09-29 11:19:23 +0100

    test: 'bbs_verify_blind_signature_proof' Java FFI

--- wrappers/java/src/main/java/life/nuggets/rs/Bbs.java
+++ wrappers/java/src/main/java/life/nuggets/rs/Bbs.java
@@ -18,6 +18,8 @@ public class Bbs {
 
   private static native String bbs_blind_signature_commitment(byte[] blinding_context);
 
+  private static native String bbs_verify_blind_signature_proof(byte[] commitment_context);
+
 
   // The rest is just regular ol' Java!
   public static void main(String[] args) {
@@ -44,6 +46,35 @@ public class Bbs {
     System.out.println("\nMissing 'messages' property:");
     String blindSigningCommitmentContext_missingMessages = "{\"public_key\":\"pQro1uqpvUPM31sr+jHffz7+KJIpA3kFen4SoKATURRgo7pk582aaqIxSinWsgHDB9j9dwxYRbC3q2ZmICR2OVMX3FHW9LZV2QAauTYFn7gEra1BSeKhdKDpzBxPjI36rAn7qKBJ+zoJjiSDxFiBlgyjPKRQzw8R6VHRJ62cUPEBUxx8mk1FpuDBdeXA8NpgAAAAA5PIYj94+VZFiDLKmgZyHmxOlO7EotGWxuSh76d51g3LhfLgz/ZvY647AiDghQwuGY5WCek2c+ag44eKZnSs3qXUCzRZsKo+r2ax3iZoaVI0+y7U4v1T+ak6CNwiLEwTvrHv85q7BeuXiARgPPsjtGuOKpHguUYfRgPGnALw6UYWTwpqhwo2/uv5IRqjVgwEkA==\",\"blinded\":[0,1]}";
     System.out.println(Bbs.bbs_blind_signature_commitment(blindSigningCommitmentContext_missingMessages.getBytes()));
+
+
+    // ----- Verify Blind Signing Commitment ----------------------------------------------------
+    
+    System.out.println("\n\n***** Verify Blind Signing Commitment *****\n");
+    
+    System.out.println("\nSuccess:");
+    String commitmentContext = "{\"public_key\":\"pQro1uqpvUPM31sr+jHffz7+KJIpA3kFen4SoKATURRgo7pk582aaqIxSinWsgHDB9j9dwxYRbC3q2ZmICR2OVMX3FHW9LZV2QAauTYFn7gEra1BSeKhdKDpzBxPjI36rAn7qKBJ+zoJjiSDxFiBlgyjPKRQzw8R6VHRJ62cUPEBUxx8mk1FpuDBdeXA8NpgAAAAA5PIYj94+VZFiDLKmgZyHmxOlO7EotGWxuSh76d51g3LhfLgz/ZvY647AiDghQwuGY5WCek2c+ag44eKZnSs3qXUCzRZsKo+r2ax3iZoaVI0+y7U4v1T+ak6CNwiLEwTvrHv85q7BeuXiARgPPsjtGuOKpHguUYfRgPGnALw6UYWTwpqhwo2/uv5IRqjVgwEkA==\",\"blinded\":[0,1],\"blinding_factor\":\"LOwrFYCZgVHuKp29PYrN7SXcki1ReqbsS7QKxHgGzZo=\",\"challenge_hash\":\"LID+l56uoBdFPrfBf574L4m8ENoJc8FFACbGV8sFGYM=\",\"commitment\":\"lZqvKXwcgNrPMdsezEa9jso6NrHFozSCOH5J0ISjZjV5+YBCHl0++odC/XYVKAV1\",\"proof_of_hidden_messages\":\"ufRyU8xKmO3d6Sz6j2497DqbBSaNhRCZz7N+WN5UeuOuKAGkrBgnCcHjGWmguVV3AAAAAxYqeDIHA16qGlSoWObxJ/+ud+STJEYU7q+QRZ8GdzTVYRcZ6bVQfJVA7SdKqRcELttuWiC2d2wNJKqBBZLDmURLl5XaK4eUt9pSzqoB/UUZ2yVoJV1O0hy4qDLxyIDzQA==\"}";
+    System.out.println(Bbs.bbs_verify_blind_signature_proof(commitmentContext.getBytes()));
+    
+    System.out.println("\nEmpty context:");
+    String commitmentContext_empty = "";
+    System.out.println(Bbs.bbs_verify_blind_signature_proof(commitmentContext_empty.getBytes()));
+    
+    System.out.println("\nEmpty context obj:");
+    String commitmentContext_emptyObj = "{}";
+    System.out.println(Bbs.bbs_verify_blind_signature_proof(commitmentContext_emptyObj.getBytes()));
+    
+    System.out.println("\nMissing 'challenge_hash' property:");
+    String commitmentContext_missingCommitment = "{\"commitment\":\"lZqvKXwcgNrPMdsezEa9jso6NrHFozSCOH5J0ISjZjV5+YBCHl0++odC/XYVKAV1\"}";
+    System.out.println(Bbs.bbs_verify_blind_signature_proof(commitmentContext_missingCommitment.getBytes()));
+    
+    System.out.println("\nMissing 'public_key' property:");
+    String commitmentContext_missingPublicKey = "{\"commitment\":\"lZqvKXwcgNrPMdsezEa9jso6NrHFozSCOH5J0ISjZjV5+YBCHl0++odC/XYVKAV1\",\"challenge_hash\":\"LID+l56uoBdFPrfBf574L4m8ENoJc8FFACbGV8sFGYM=\"}";
+    System.out.println(Bbs.bbs_verify_blind_signature_proof(commitmentContext_missingPublicKey.getBytes()));
+    
+    System.out.println("\nMissing 'proof_of_hidden_messages' property:");
+    String commitmentContext_missingProofOfHidden = "{\"public_key\":\"pQro1uqpvUPM31sr+jHffz7+KJIpA3kFen4SoKATURRgo7pk582aaqIxSinWsgHDB9j9dwxYRbC3q2ZmICR2OVMX3FHW9LZV2QAauTYFn7gEra1BSeKhdKDpzBxPjI36rAn7qKBJ+zoJjiSDxFiBlgyjPKRQzw8R6VHRJ62cUPEBUxx8mk1FpuDBdeXA8NpgAAAAA5PIYj94+VZFiDLKmgZyHmxOlO7EotGWxuSh76d51g3LhfLgz/ZvY647AiDghQwuGY5WCek2c+ag44eKZnSs3qXUCzRZsKo+r2ax3iZoaVI0+y7U4v1T+ak6CNwiLEwTvrHv85q7BeuXiARgPPsjtGuOKpHguUYfRgPGnALw6UYWTwpqhwo2/uv5IRqjVgwEkA==\",\"commitment\":\"lZqvKXwcgNrPMdsezEa9jso6NrHFozSCOH5J0ISjZjV5+YBCHl0++odC/XYVKAV1\",\"challenge_hash\":\"LID+l56uoBdFPrfBf574L4m8ENoJc8FFACbGV8sFGYM=\"}";
+    System.out.println(Bbs.bbs_verify_blind_signature_proof(commitmentContext_missingProofOfHidden.getBytes()));
 
   }
 }
