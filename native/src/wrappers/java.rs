e322716531478e9036d46300e9ffc7f198190585
Andrew Lord <andrew.lord@appliedblockchain.com>
2022-10-06 10:59:48 +0100

    feat: add 'bls_generate_g1_key' java FFI

--- native/src/wrappers/java.rs
+++ native/src/wrappers/java.rs
@@ -1327,6 +1327,59 @@ pub extern "system" fn Java_life_nuggets_rs_Bbs_bls_1generate_1blinded_1g2_1key(
     Err(_) => { handle_err!("Failed to stringify blinded G2 key", env); }
   }
 }
+
+#[allow(non_snake_case)]
+#[no_mangle]
+pub extern "system" fn Java_life_nuggets_rs_Bbs_bls_1generate_1g1_1key(
+  env: JNIEnv,
+  _class: JClass,
+  ctx: jbyteArray,
+) -> jstring {
+  let context_bytes;
+  match env.convert_byte_array(ctx) {
+      Err(_) => panic!("Failed converting `ctx` to byte array"),
+      Ok(bc) => context_bytes = bc,
+  };
+
+  // convert JSON string to JSON
+  let context_json: Value = match String::from_utf8(context_bytes.to_vec()) {
+    Ok(context_string) => {
+      match serde_json::from_str(&context_string) {
+        Ok(context) => context,
+        Err(_) => { handle_err!("Failed parsing JSON context", env); }
+      }
+    },
+    Err(_) => { handle_err!("Context not set", env); }
+  };
+
+  // convert seed base64 string to slice
+  let (pk_bytes, sk_bytes) = match context_json["seed"].as_str() {
+    Some(seed) => {
+      match base64::decode(seed) {
+        Ok(seed_bytes) => bls_generate_g1_key(Some(seed_bytes)),
+        Err(_) => { handle_err!("Failed decoding base64 for: 'seed'", env); }
+      }
+    },
+    None => bls_generate_g1_key(None)
+  };
+
+  let g1_key = json!({
+    "public_key": base64::encode(pk_bytes.as_slice()),
+    "secret_key": base64::encode(sk_bytes.as_slice()),
+  });
+
+  // Serialize `BlindCommitmentContext` to a JSON string
+  match serde_json::to_string(&g1_key) {
+    Ok(g1_key_string) => {
+      let output = env
+          .new_string(g1_key_string)
+          .expect("Unable to create string from G1 key data");
+    
+      output.into_inner()
+    },
+    Err(_) => { handle_err!("Failed to stringify G1 key", env); }
+  }
+}
 #[allow(non_snake_case)]
 #[no_mangle]
 pub extern "system" fn Java_life_nuggets_rs_Bbs_bbs_1blind_1signature_1commitment(
