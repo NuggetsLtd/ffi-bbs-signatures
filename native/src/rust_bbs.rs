2fbc11b4597ef7680bf75cb0d786016cfc2fba0c
Andrew Lord <andrew.lord@appliedblockchain.com>
2022-10-06 10:05:50 +0100

    feat: create generic 'rust_bbs_create_proof' fn

--- native/src/rust_bbs.rs
+++ native/src/rust_bbs.rs
@@ -1,18 +1,7 @@
 use std::collections::{BTreeMap,BTreeSet};
 use serde::{Deserialize, Serialize};
-use bbs::prelude::{
-  ProofNonce,
-  SignatureMessage,
-  SecretKey,
-  PublicKey,
-  Prover,
-  BBSError,
-  BlindSignatureContext,
-  SignatureBlinding,
-  Commitment,
-  BlindSignature,
-  Signature,
-};
+use bbs::prelude::*;
+use bbs::{FR_COMPRESSED_SIZE};
 
 #[derive(Serialize, Deserialize)]
 pub struct BlindingContext {
@@ -107,4 +96,67 @@ pub fn rust_bbs_verify(
   }
 
   signature.verify(messages.as_slice(), public_key)
+}
+
+#[allow(dead_code)]
+pub fn rust_bbs_create_proof(
+  signature: &Signature,
+  public_key: &PublicKey,
+  messages: &Vec<ProofMessage>,
+  revealed: &BTreeSet<usize>,
+  nonce: Option<Vec<u8>>,
+) -> Result<Vec<u8>, BBSError> {
+  // check public key is valid
+  if public_key.validate().is_err() {
+      panic!("Invalid public key");
+  }
+
+  let mut bitvector = (messages.len() as u16).to_be_bytes().to_vec();
+  bitvector.append(&mut revealed_to_bitvector(messages.len(), &revealed));
+  
+  let pok = match PoKOfSignature::init(
+    &signature,
+    &public_key,
+    &messages.as_slice()
+  ) {
+    Ok(pok) => pok,
+    Err(error) => {
+      return Err(BBSError::from(BBSErrorKind::GeneralError {
+        msg: format!("Failed generating proof of knowledge: {}", error),
+      }))
+    }
+  };
+
+  let mut challenge_bytes = pok.to_bytes();
+  if let Some(b) = nonce {
+    challenge_bytes.extend_from_slice(&ProofNonce::hash(b.as_slice()).to_bytes_compressed_form());
+  } else {
+    challenge_bytes.extend_from_slice(&[0u8; FR_COMPRESSED_SIZE]);
+  }
+
+  let challenge_hash = ProofChallenge::hash(&challenge_bytes);
+  match pok.gen_proof(&challenge_hash) {
+    Ok(proof) => {
+      bitvector.extend_from_slice(proof.to_bytes_compressed_form().as_slice());
+      Ok(bitvector)
+    },
+    Err(_) => Err(BBSError::from(BBSErrorKind::GeneralError {
+      msg: "Failed generating proof".to_string(),
+    }))
+  }
+}
+
+/// Expects `revealed` to be sorted
+fn revealed_to_bitvector(total: usize, revealed: &BTreeSet<usize>) -> Vec<u8> {
+    let mut bytes = vec![0u8; (total / 8) + 1];
+
+    for r in revealed {
+        let idx = *r / 8;
+        let bit = (*r % 8) as u8;
+        bytes[idx] |= 1u8 << bit;
+    }
+
+    // Convert to big endian
+    bytes.reverse();
+    bytes
 }
